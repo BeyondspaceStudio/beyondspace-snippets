569b2e6bc40f60738e7176a5c551eff174c47edb
BeyondspaceStudio <lamphamngoc99@gmail.com>
2023-05-20 03:35:31 +0000

    - Bump style for highlight code

--- src/highlight-block/index.js
+++ src/highlight-block/index.js
@@ -15,7 +15,7 @@ loadjs=window.loadjs || function(){var h=function(){},o={},c={},f={};function u(
     const defaultSettings = {
         style: 'github'
     }
-    
+
     // Expect data-widget-config attribute
     const scriptOption = JSON.parse(currentscript.dataset.widgetConfig || "{}");
 
@@ -61,6 +61,9 @@ loadjs=window.loadjs || function(){var h=function(){},o={},c={},f={};function u(
             height: 25px;
             display: inline-block;
             padding: 0;
+        }
+        code.hljs {
+            padding: 1em;
         }
         `
         document.head.appendChild(style);
