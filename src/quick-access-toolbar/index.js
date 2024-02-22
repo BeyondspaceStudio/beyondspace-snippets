d38ef6239ca32dad8de9da35fcdac2fe6efec981
Beyondspace <lamphamngoc99@gmail.com>
2024-02-22 16:28:10 +0700

    Update index.js
    - Update debugMode with `isBeyondspaceDebugMode`

--- src/quick-access-toolbar/index.js
+++ src/quick-access-toolbar/index.js
@@ -10,7 +10,7 @@ loadjs=window.loadjs || function(){var h=function(){},o={},c={},f={};function u(
         document.addEventListener("DOMContentLoaded", fn.bind(this, currentscript));
     }
 })(function(currentscript) {
-    const debugMode = new URLSearchParams(window.location.search).get('beyondspace-debug') !== null;
+    const debugMode = new URLSearchParams(window.location.search).get('beyondspace-debug') !== null || window.isBeyondspaceDebugMode;
 
     const deps = [{
         path: 'https://cdn.jsdelivr.net/gh/csuwildcat/SelectorListener/selector-listeners.min.js',
