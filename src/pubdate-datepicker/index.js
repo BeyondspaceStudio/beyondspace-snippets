ae06b9102767128ab003e298a573ac44bc38ec52
Beyondspace <lamphamngoc99@gmail.com>
2024-03-09 12:53:21 +0700

    Update index.js
    Update bundle name pubdate-datepicker-deps

--- src/pubdate-datepicker/index.js
+++ src/pubdate-datepicker/index.js
@@ -36,7 +36,7 @@ loadjs=window.loadjs || function(){var h=function(){},o={},c={},f={};function u(
 
         loadjs([
             debugMode ? 'https://localhost:3000/static/js/bundle.js' : 'https://beyondspace-admin.pages.dev/blogpub-datepicker/main.js?t=' + Date.now(),
-        ], 'foobar');
+        ], 'pubdate-datepicker-deps');
     });
 });
 
