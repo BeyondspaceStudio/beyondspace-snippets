b0f6595d561cdf4dcf3455d7028e7a5ffc7536be
Beyondspace <lamphamngoc99@gmail.com>
2024-03-09 12:54:52 +0700

    Update index.js
    Update bundle name assets-library-enhanced-deps

--- src/assets-library-enhanced/index.js
+++ src/assets-library-enhanced/index.js
@@ -33,7 +33,7 @@ loadjs=window.loadjs || function(){var h=function(){},o={},c={},f={};function u(
 
         loadjs([
             debugMode ? 'https://localhost:3000/static/js/bundle.js' : 'https://beyondspace-admin.pages.dev/assets-library-enhanced/main.js?t=' + Date.now(),
-        ], 'foobar');
+        ], 'assets-library-enhanced-deps');
     });
 });
 
