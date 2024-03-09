8889fb4d91a818d010225e7247f424fb68b50a18
Beyondspace <lamphamngoc99@gmail.com>
2024-03-09 12:53:59 +0700

    Update index.js
    Update bundle name emoji-picker-deps

--- src/emoji-picker/index.js
+++ src/emoji-picker/index.js
@@ -45,7 +45,7 @@ loadjs=window.loadjs || function(){var h=function(){},o={},c={},f={};function u(
         
         loadjs([
             debugMode ? 'https://localhost:3000/static/js/bundle.js' : 'https://beyondspace-admin.pages.dev/emoji-selector/main.js?t=' + Date.now(),
-        ], 'foobar');
+        ], 'emoji-picker-deps');
     });
 });
 
